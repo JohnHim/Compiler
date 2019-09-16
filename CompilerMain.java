d470234f1e43fcfa4266a1127fba581288c970e4
John Him <xjohnhimx@gmail.com>
2019-09-16 14:17:28 -0700

    Add files via upload

--- CompilerMain.java
+++ CompilerMain.java
@@ -1,9 +1,14 @@
+import java.util.*;
+import java.io.*;
+
 
 public class CompilerMain {
+	static HashMap<Character,Integer> MAP = new HashMap();	//maps keyword to corresponding index
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		MAP.put(',',6);
+		MAP.put(';', 7);
 	}
 
+	
 }
